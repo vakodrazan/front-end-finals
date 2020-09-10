7b001b0b70c1d3a79199d115a6503c6c1f4e1dc4
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-10 16:26:43 +0300

    Remove the adding remove class because it has been removed

--- image_slider.js
+++ image_slider.js
@@ -8,11 +8,16 @@ const clickImage = e => {
     image.classList.add('open');
     const cloneImage = image.cloneNode(true); // clone the element
     slider.appendChild(cloneImage); // take the clone version then append it in another element
+    cloneImage.classList.remove('remove')
 
     const removeChild = slider.removeChild(slider.firstElementChild); // remove the element that is already set there before
 
     console.log("You click this image!", e.target)
   }
+  //  else {
+  //   sliderImage.classList.add('remove')
+  //   sliderImage.classList.remove('open')
+  // }
 }
 
 sliderImage.forEach(image => image.addEventListener("click", clickImage));
