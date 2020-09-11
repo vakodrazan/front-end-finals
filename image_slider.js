1cb2efc654ae942a963d67c7761f8d7c93fc3586
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-11 07:43:45 +0300

    Trying to work on the image which is not clicked

--- image_slider.js
+++ image_slider.js
@@ -3,21 +3,15 @@ console.log(sliderImage);
 const slider = document.querySelector('.slider');
 
 const clickImage = e => {
-  if (e.target.matches('img')) {
-    const image = event.target;
+  if (e.target.matches('img') === true) {
+    const image = e.target;
     image.classList.add('open');
     const cloneImage = image.cloneNode(true); // clone the element
     slider.appendChild(cloneImage); // take the clone version then append it in another element
-    cloneImage.classList.remove('remove')
-
     const removeChild = slider.removeChild(slider.firstElementChild); // remove the element that is already set there before
-
     console.log("You click this image!", e.target)
   }
-  //  else {
-  //   sliderImage.classList.add('remove')
-  //   sliderImage.classList.remove('open')
-  // }
+
 }
 
 sliderImage.forEach(image => image.addEventListener("click", clickImage));
