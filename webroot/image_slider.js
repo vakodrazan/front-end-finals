701d00a8baf7fa97824f855d32e0544f0abbd4b3
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-12-11 07:01:43 +0300

    Only add the opacity when it hovered

--- webroot/image_slider.js
+++ webroot/image_slider.js
@@ -5,7 +5,6 @@ const slider = document.querySelector('.slider');
 const clickImage = e => {
   if (e.target.matches('img') === true) {
     const image = e.target;
-    image.classList.add('open');
     const cloneImage = image.cloneNode(true); // clone the element
     cloneImage.classList.add('imageViewing')
     slider.appendChild(cloneImage); // take the clone version then append it in another element
