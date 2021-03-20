0beeae0cc33303b46e4e481d95ad669ef9ebdf8d
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-03-20 09:13:29 +0300

    Fixed the image slider

--- webroot/image_slider.js
+++ webroot/image_slider.js
@@ -1,16 +1,17 @@
-const sliderImage = document.querySelectorAll('.sliderImage');
-console.log(sliderImage);
-const slider = document.querySelector('.slider');
+const imageCollection = document.querySelectorAll('.sliderImage');
 
-const clickImage = e => {
-  if (e.target.matches('img') === true) {
-    const image = e.target;
-    const cloneImage = image.cloneNode(true); // clone the element
-    cloneImage.classList.add('imageViewing')
-    slider.appendChild(cloneImage); // take the clone version then append it in another element
-    const removeChild = slider.removeChild(slider.firstElementChild); // remove the element that is already there before
-  }
+const activeImage = document.querySelector('.image-active');
+const firstImage = document.querySelector('.open');
+const allImages = Array.from(document.querySelectorAll('.section_image-slider__gallery__image'));
 
+// Handling the slider
+const imageSlider = (e) => {
+  const clickedImage = e.target;
+   activeImage.src = clickedImage.src
+   firstImage.classList.remove('open')
+   imageCollection.forEach(image => image.style.opacity = 0.5);
+   clickedImage.style.opacity = 1;
 }
 
-sliderImage.forEach(image => image.addEventListener("click", clickImage));
+// Event listner for images by looping through them
+imageCollection.forEach(image => image.addEventListener(('click'), imageSlider));
