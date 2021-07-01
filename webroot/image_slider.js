48931e5b145f515b2bfddc66748996133b051d53
Noeline <noeline.mar@onja.org>
2021-07-01 22:09:41 +0300

    Changing the image address of the arches photograph

--- webroot/image_slider.js
+++ webroot/image_slider.js
@@ -1,18 +1,23 @@
-const imageCollection = document.querySelectorAll('.sliderImage');
-const sliderImage = document.querySelectorAll('.sliderImage');
+const imageCollection = document.querySelectorAll(".sliderImage");
+const sliderImage = document.querySelectorAll(".sliderImage");
 
-const activeImage = document.querySelector('.image-active');
-const firstImage = document.querySelector('.open');
-const allImages = Array.from(document.querySelectorAll('.section_image-slider__gallery__image'));
+const activeImage = document.querySelector(".image-active");
+const firstImage = document.querySelector(".open");
+const allImages = Array.from(
+  document.querySelectorAll(".section_image-slider__gallery__image")
+);
 
 // Handling the slider
 const imageSlider = (e) => {
   const clickedImage = e.target;
-   activeImage.src = clickedImage.src
-   sliderImage.forEach(image => image.style.opacity = 0.5);
-   firstImage.classList.remove('open');
-   clickedImage.style.opacity = 1;
-}
+  activeImage.src = clickedImage.src;
+  activeImage.alt = clickedImage.alt;
+  sliderImage.forEach((image) => (image.style.opacity = 0.5));
+  firstImage.classList.remove("open");
+  clickedImage.style.opacity = 1;
+};
 
 // Event listner for images by looping through them
-imageCollection.forEach(image => image.addEventListener(('click'), imageSlider));
+imageCollection.forEach((image) =>
+  image.addEventListener("click", imageSlider)
+);
